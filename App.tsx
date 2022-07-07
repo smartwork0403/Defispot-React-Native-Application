d6fa4cdbf351a5e58f552fb09ab5542615311526
webdev0415 <igor.shevk@gmail.com>
2022-07-07 18:05:55 +0430

    Add splash screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import RNBootSplash from 'react-native-bootsplash';
 
 import Navigation from './src/components/Navigation';
 
 const App = () => {
   return (
-    <NavigationContainer theme={{colors: {background: '#F5F5F5'}}}>
+    <NavigationContainer
+      theme={{colors: {background: '#F5F5F5'}}}
+      onReady={() => RNBootSplash.hide({fade: true})}>
       <Navigation />
     </NavigationContainer>
   );
