27899edc44dc23673fa30676d6ef26c73073406c
Mohammad Foroughi <mohammadforoughi@Mohammads-MacBook-Pro.local>
2022-07-14 19:24:46 +0430

    funcdev: fix build issues

--- ios/DefispotReactNative/AppDelegate.h
+++ ios/DefispotReactNative/AppDelegate.h
@@ -1,21 +1,7 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-#import "RNBootSplash.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
-
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-
-  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView]; // <- initialization using the storyboard file name
-
-  return YES;
-}
 
 @property (nonatomic, strong) UIWindow *window;
 
