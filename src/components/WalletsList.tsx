7c10586e1ff0b8a3f0519ac5008a9329fe61910d
webdev0415 <igor.shevk@gmail.com>
2022-07-17 14:14:48 +0430

    uidev | add profile screen

--- src/components/WalletsList.tsx
+++ src/components/WalletsList.tsx
@@ -1,37 +1,23 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Pressable} from 'react-native';
-import Collapsible from 'react-native-collapsible';
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 
 import CustomText from './CustomText';
 import Asset from './Asset';
 
-import ChevronDownSvg from '../assets/icons/chevron-down.svg';
+import CollapsibleCard from './CollapsibleCard';
 
 const Item: React.FC = () => {
-  const [isCollapsed, setIsCollapsed] = useState(true);
-
   return (
-    <View style={styles.item}>
-      <View style={styles.top}>
-        <Asset name="USD" value="25.0%" horizontal />
-        <CustomText style={styles.topValue}>25.000</CustomText>
-
-        <Pressable
-          style={styles.topButton}
-          onPress={() => setIsCollapsed(!isCollapsed)}>
-          <ChevronDownSvg
-            height={4}
-            width={7}
-            color="#121315"
-            style={{transform: [{rotate: isCollapsed ? '0deg' : '180deg'}]}}
-          />
-        </Pressable>
-      </View>
-
-      <Collapsible style={styles.bottom} collapsed={isCollapsed}>
-        <CustomText>collapsed content</CustomText>
-      </Collapsible>
-    </View>
+    <CollapsibleCard
+      style={styles.item}
+      top={
+        <>
+          <Asset name="USD" value="25.0%" horizontal />
+          <CustomText style={styles.itemValue}>25.000</CustomText>
+        </>
+      }
+      bottom={<CustomText>collapsed content</CustomText>}
+    />
   );
 };
 
@@ -49,33 +35,10 @@ const styles = StyleSheet.create({
   item: {
     marginBottom: 8,
   },
-  top: {
-    backgroundColor: '#fff',
-    paddingTop: 16,
-    paddingBottom: 16,
-    paddingRight: 16,
-    paddingLeft: 16,
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#EFF0F3',
-  },
-  topValue: {
+  itemValue: {
     marginLeft: 'auto',
     fontFamily: 'Inter-Medium',
   },
-  topButton: {
-    marginLeft: 12,
-    height: 24,
-    width: 24,
-    borderRadius: 24 / 2,
-    backgroundColor: '#EFF0F3',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bottom: {backgroundColor: 'red'},
 });
 
 export default WalletsList;
