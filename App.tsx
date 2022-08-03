31b3aca7324f6d92cb72a415715fc4f480bbfe94
Mohammad <mohamad.foroughi@yahoo.com>
2022-08-03 19:00:32 +0430

    funcdev: add expo support

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import RNBootSplash from 'react-native-bootsplash';
+// import RNBootSplash from 'react-native-bootsplash';
 
 import Navigation from './src/components/Navigation';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 const App = () => {
+  const [fontloaded, setfontloaded] = useState(false);
+
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      'CircularStd-Bold': require('./src/assets/fonts/CircularStd-Bold.ttf'),
+      'CircularStd-Medium': require('./src/assets/fonts/CircularStd-Medium.ttf'),
+      'CircularStd-Regular': require('./src/assets/fonts/CircularStd-Regular.ttf'),
+      'Inter-Bold': require('./src/assets/fonts/Inter-Bold.ttf'),
+      'Inter-Medium': require('./src/assets/fonts/Inter-Medium.ttf'),
+      'Inter-Regular': require('./src/assets/fonts/Inter-Regular.ttf'),
+      'Inter-SemiBold': require('./src/assets/fonts/Inter-SemiBold.ttf'),
+    });
+  };
+  if (!fontloaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setfontloaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
   return (
-    <NavigationContainer
-      theme={{colors: {background: '#F7F8FA'}}}
-      onReady={() => RNBootSplash.hide({fade: true})}>
+    <NavigationContainer theme={{colors: {background: '#F7F8FA'}}}>
       <Navigation />
     </NavigationContainer>
   );
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import {AppRegistry, LogBox} from 'react-native';
+import {registerRootComponent} from 'expo';
+import {AppRegistry, LogBox, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 LogBox.ignoreLogs(['Require cycle: node_modules/victory-vendor']);
 
-AppRegistry.registerComponent(appName, () => App);
+// if (Platform.OS == 'android') {
+registerRootComponent(App);
+// } else {
+//   AppRegistry.registerComponent(appName, () => App);
+// }
